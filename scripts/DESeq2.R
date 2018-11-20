de27d1b19c84f80454d9158f742d49c9a5138ff6
Marc Galland <m.galland@uva.nl>
2018-11-20 13:32:43 +0100

    updated deseq script to deal with multiple conditions

--- scripts/DESeq2.R
+++ scripts/DESeq2.R
@@ -1,25 +1,49 @@
-# retrieving conditions from command line arguments
-args <- commandArgs(trailingOnly=TRUE)
-# Import data from featureCounts
+#!/usr/bin/env Rscript
+library(DESeq2)
+library("optparse")
 
-countdata <- read.table("results/counts.txt", header=TRUE, row.names=1)
-# Remove first five columns (chr, start, end, strand, length)
-countdata <- countdata[ ,6:ncol(countdata)]
+# arguments to provide
+option_list = list(
+  make_option(c("-c", "--counts"), type="character", default="results/counts.txt", help="counts tabulated file from Feature Counts", metavar="character"),
+  make_option(c("-s", "--samplefile"), type="character", default=NULL, help="sample files used to get conditions for DESEq2 model fit", metavar="character"),
+  make_option(c("-o", "--outdir"), type="character", default="results/deseq/diff.tsv", help="where to place differential expression files", metavar="character")
+) 
+
+# parse the command-line arguments and pass them to a list called 'opt'
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+######## Import and wrangle counts produced by FeatureCounts #######
+countdata <- read.table(opt$counts, header=TRUE, row.names=1,stringsAsFactors = F)
+countdata <- countdata[ ,6:ncol(countdata)] # Remove first five columns (chr, start, end, strand, length)
 
 # Remove .bam or .sam from filenames
 colnames(countdata) <- gsub("\\.[sb]am$", "", colnames(countdata))
-colnames(countdata) <- gsub("\\mapped.Pehyb_Quatt", "", colnames(countdata))
+
 # Convert to matrix
 countdata <- as.matrix(countdata)
 
-# Assign condition (first four are controls, second four contain the expansion)
+######### Extract experimental conditions from the file specifying the samples
+samplefile = read.delim(file = opt$samplefile,header = T,stringsAsFactors = F)
+col.names = colnames(samplefile)                                                # extract all column names
+columns.to.discard = c("sample","fq1","fq2")                                    # column we don't need to extract all conditions
+conditions = col.names[! col.names %in% columns.to.discard]                     # only keeping the column of interest
+
+# one condition
+if (length(conditions) == 1){
+  condition <- factor(samplefile[,"conditions"])
+# two conditions
+} else if (length(conditions == 2)){
+  # two conditions --> make a third column that is the product of the two
+  samplefile$condition = with(samplefile,paste(condition[1],condition[2],sep = "-"))
+} else if (length(conditions > 2)){
+   print("too many conditions to compare, skipping")
+}
+
 (condition <- factor(args[2:length(args)]))
 #(condition <- factor(c(rep("ctl", 3), rep("exp", 3))))
 
 # Analysis with DESeq2 ----------------------------------------------------
-
-library(DESeq2)
-
 # Create a coldata frame and instantiate the DESeqDataSet. See ?DESeqDataSetFromMatrix
 (coldata <- data.frame(row.names=colnames(countdata), condition))
 dds <- DESeqDataSetFromMatrix(countData=countdata, colData=coldata, design=~condition)
