a41076cffa2a2338e0f8da346d7ef8eb0746c04c
tijs bliek <bliek@uva.nl>
2018-12-13 15:44:36 +0100

    updated and fixed DE_with_function.py

--- scripts/DE_with_Function.py
+++ scripts/DE_with_Function.py
@@ -2,42 +2,45 @@ import os
 import sys
 
 args = sys.argv
-
-fa = open("../" + args[1], "r")
+fa = open(args[1], "r")
 name = ""
 names = {}
 combis = {}
 for l in fa:
-    if l.startswith(">"):
-        l = l.split(" ")
-        if len(names)>0:
-            names[name] = [str(len(names[name])), names[name]]
-        name = ".".join(l[1].split(".")[:-1])
-        no = l[0][1:]
-        combis[no] = name
-        names[name] = ""
-    else:
-        names[name] += l.rstrip()
+    if len(l) > 5:
+        if l.startswith(">"):
+            l = l.split(" ")
+            if len(names)>0:
+                names[name] = [str(len(names[name])), names[name]]
+            name = ".".join(l[1].split(".")[:-1])
+            no = l[0][1:]
+            combis[no] = name
+            names[name] = ""
+        else:
+            names[name] += l.rstrip()
 names[name] = [str(len(names[name])), names[name]]
 combis[no] = name
 fa.close()
 
-fu = open("../" + args[2], "r")
+lijst = []
+fu = open(args[2], "r")
 for l in fu:
     l = l.rstrip().split("\t")
-    names[combis[l[0]]] = l[1:]
+    if combis[l[0]] not in lijst:
+        names[combis[l[0]]] += l[2:]
+        lijst.append(combis[l[0]])
 fu.close()
+print(combis[l[0]])
+inFile  = open(args[3], "r")
+uitFile = open(args[4], "w")
 
-inFile  = open("../" + args[3], "r")
-uitFile = open("../" + args[4], "w")
-print(names["STRG.447"])
 for l in inFile:
-    l = l.rstrip().split(",")
-    if "Gene" in l[1]:
-        print("check")
-        l.append("length\tlength of hit\te-value\tname and function")
+    l = l.rstrip().split("\t")
+    if "genes" in l[0]:
+        l.append("length\tsequence\tlength of hit\te-value\tname and function")
     else:
-        if len(l) > 3:
-            l.append("\t".join(names[l[1].strip("\"")]))
+        if l[0] in names:
+            l.append("\t".join(names[l[0]]))
     uitFile.write("\t".join(l)+"\n")
-
+inFile.close()
+uitFile.close()
