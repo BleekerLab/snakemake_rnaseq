79d579f78fb820530209c36988f681a19377dcd1
tijs bliek <bliek@uva.nl>
2019-05-22 13:32:07 +0200

    removed blast functions and added Functional annotation

--- scripts/DE_with_Function.py
+++ scripts/DE_with_Function.py
@@ -1,4 +1,5 @@
 from optparse import OptionParser 
+import glob
 
 # get the command line arguments
 parser = OptionParser(description="Script that combines the normalized and differential expression data as outputted by DESeq2.R, the clusters as outputted by plotscript.R and the results from the blast against the reference proteome. Resulting in a combined tab-delimeted data file")
@@ -7,24 +8,19 @@ parser.add_option('-r', '--result_file',
                     default="results/result.csv",
                     metavar="",
                     help = "path and name of of the input file, being the output file of the R-script DESeq2.R, default = results/result.csv")
-parser.add_option('-f', '--fasta_file', 
+parser.add_option('-a', '--anno_file', 
                     type=str,
-                    default="genome/stringtie_transcriptome.fasta",
+                    default="annotations/Petunia_axillaris_v1.6.2_proteins.mapman_annotation.txt",
                     metavar="",
                     help = "path and name of the stringtie transcriptome fasta file, a file containing the fastas of the de novo generated stringtie transcriptome, default = result/helperFile.csv")
-parser.add_option('-b', '--blast_file', 
-                    type=str,
-                    default="results/stringtie_transcriptome_blast.txt",
-                    metavar="",
-                    help = "path and name of of the blast file, a tab delimited file containing the blast result for the de novo transcriptome, default = results/result.csv")
 parser.add_option('-c', '--cluster_file', 
                     type=str,
-                    default="result/clusters.csv",
+                    default="results/clusters.txt",
                     metavar="",
                     help = "path and name of the clusters file, a tab delimited file containing cluster numbers and depending on the method of clustering correlation values or membership values., default = result/helperFile.csv")
 parser.add_option('-o', '--output_file', 
                     type=str,
-                    default="result/final.txt",
+                    default="results/final.txt",
                     metavar="",
                     help = "path and name of the output file. A tab delimimited file containing normalised reads, differential expressions and (adjusted)p-values, number of clusters the genes partisioned to and the hypothetical function as found by a blast. default = result/final.txt")
 parser.add_option('-p', '--working_dir', 
@@ -36,37 +32,6 @@ parser.add_option('-p', '--working_dir',
 
 (options, args) = parser.parse_args()
 
-# get combination of numbers and transcript name.
-fa = open(options.fasta_file, "r")
-name = ""
-names = {}
-combis = {}
-for l in fa:
-    if len(l) > 5:
-        if l.startswith(">"):
-            l = l.split(" ")
-            if len(names)>0:
-                names[name] = [str(len(names[name])), names[name]]
-            name = ".".join(l[1].split(".")[:-1])
-            no = l[0][1:]
-            combis[no] = name
-            names[name] = ""
-        else:
-            names[name] += l.rstrip()
-names[name] = [str(len(names[name])), names[name]]
-combis[no] = name
-fa.close()
-
-# get 
-lijst = []
-fu = open(options.blast_file, "r")
-for l in fu:
-    l = l.rstrip().split("\t")
-    if combis[l[0]] not in lijst:
-        names[combis[l[0]]] += l[2:]
-        lijst.append(combis[l[0]])
-fu.close()
-
 clusts = open(options.cluster_file, "r")
 clusters = {}
 for line in clusts:
@@ -74,24 +39,58 @@ for line in clusts:
     line = line.split("\t")
     if len(line) == 2:
         line.append("NaN")
-    clusters[line[0]] = "\t".join(line[1:])
+    clusters[line[0].lower()] = "\t".join(line[1:])
+
 clusts.close()
 
-inFile  = open(options.result_file, "r")
-uitFile = open(options.output_file, "w")
-for l in inFile:
-    l = l.rstrip().split("\t")
-    if "genes" in l[0]:
-        l = [x.replace(options.working_dir, "") for x in l]    # remove path from sample names
-        l.append("\t".join([clusters["gene"], "length\tsequence\tlength of hit\te-value\tname and function"]))
-    elif l[0] in clusters:
-        if l[0] in names:
-            l.append(clusters[l[0]] + "\t".join(names[l[0]]))
+# if there is an annotation file available add annotaions.
+if options.anno_file in glob.glob(options.anno_file):
+    fa = open(options.anno_file, "r")
+    annos = {}
+    count = 0
+    for l in fa:
+        l = l.split("\t")
+        l = [x.strip("'") for x in l]
+        if len(l)>2:
+            if len(l[2])>1:
+                if l[2].lower() not in annos:
+                    annos[l[2].lower()] = ".".join(l[1].split(".")[:3]).replace(" ", "_")
+                else:
+                    annos[l[2].lower()] += (";" + ".".join(l[1].split(".")[:3]).replace(" ", "_"))
+    fa.close()
+    
+    inFile  = open(options.result_file, "r")
+    uitFile = open(options.output_file, "w")
+    for l in inFile:
+        l = l.rstrip().split("\t")
+        if "genes" in l[0]:
+            l = [x.replace(options.working_dir, "") for x in l]    # remove path from sample names
+            l.append("\t".join([clusters["gene"], "annotation"]))
+        elif l[0].lower() in clusters:
+            if l[0].lower() in annos:
+                l.append(clusters[l[0].lower()] + "\t" + annos[l[0].lower()])
+            else:
+                l.append(clusters[l[0].lower()] + "\tno annotation available.")
+        elif l[0].lower() in annos:
+            l.append("NaN\tNaN\t" + annos[l[0].lower()])
         else:
-            l.append(clusters[l[0]] + "\tWas not included in the blast.")
-    else:
-        if l[0] in names:
-            l.append("NaN\tNaN\t"+ "\t".join(names[l[0]]))
-    uitFile.write("\t".join(l)+"\n")
-inFile.close()
-uitFile.close()
+            l.append("NaN\tNaN\tno annotation available.")
+        uitFile.write("\t".join(l)+"\n")
+    inFile.close()
+    uitFile.close()
+
+else:
+    inFile  = open(options.result_file, "r")
+    uitFile = open(options.output_file, "w")
+    for l in inFile:
+        l = l.rstrip().split("\t")
+        if "genes" in l[0]:
+            l = [x.replace(options.working_dir, "") for x in l]    # remove path from sample names
+            l.append("\t".join([clusters["gene"]]))
+        elif l[0].lower() in clusters:
+            l.append(clusters[l[0].lower()])
+        else:
+            l.append("NaN\tNaN")
+        uitFile.write("\t".join(l)+"\n")
+    inFile.close()
+    uitFile.close()
