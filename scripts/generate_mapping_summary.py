c2565bcbca8e3bbc3ba3c8dececb692c9a16a731
Marc Galland <m.galland@uva.nl>
2020-09-15 12:02:34 +0200

    try to fix symlink issue

--- scripts/generate_mapping_summary.py
+++ scripts/generate_mapping_summary.py
@@ -1,54 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import pandas as pd
 import os
 from functools import reduce
-from optparse import OptionParser
+import sys
 
-
-####
-#CLI
-####
-parser = OptionParser()
-parser.add_option("-d", "--directory", default=None, dest="directory",
-                  help="directory with the STAR mapping reports (*Log.final.out).")
-parser.add_option("-o", "--outfile", default="None", dest="outfile", 
-	              help="Tabulated separated file where mapping summaries for all samples will be written.") 
-(options, args) = parser.parse_args()
-
-
+directory_with_mapping_reports = sys.argv[1]
+mapping_summary = sys.argv[2]
 
 ############################################################
 # Reads each file. Add sample name in the column with values
 ############################################################
 
-list_of_logs = [f for f in os.listdir(options.directory) if f.endswith("Log.final.out")]
+list_of_logs = [f for f in os.listdir(directory_with_mapping_reports) if f.endswith("Log.final.out")]
 list_of_logs.sort()
 
 
-
-
 sample_names = [log.replace("_Log.final.out","") for log in list_of_logs]
-list_of_dfs = [pd.read_csv(
-	log, 
-	sep = "\t", 
-	names=["attribute", str(sample)]) 
-  for log,sample in zip(list_of_logs,sample_names)
+list_of_dfs = [pd.read_csv(log, sep = "\t", names=["attribute", str(sample)]) 
+  for log,sample in zip(list_of_logs, sample_names)
 ]
-list_of_dfs[0].head()
-
-
-# In[17]:
 
 
 df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['attribute'],
                                             how='outer'), list_of_dfs)
-
-df_merged.to_csv(options.outfile,sep="\t")
+df_merged.to_csv(mapping_summary, sep="\t")
 
 
 
